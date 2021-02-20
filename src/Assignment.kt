3296606edf96f1f4530df84a516f0896bdb6d873
irenenyatichi <irenenyatichik@gmail.com>
2021-02-20 23:33:28 +0300

    assignment1

---
+++ src/Assignment.kt
@@ -0,0 +1,12 @@
+fun main() {
+    val student= arrayOf("ireneNyatichi","0712345678","false",60.12345,20)
+    val name=("irene_Nyatichi")
+    print(name)
+    var age= 20
+    print(age)
+    val number=("0712345678")
+    print(number)
+    var weight= 60.12345
+    print(weight)
+    val kenyan= (false)
+}
