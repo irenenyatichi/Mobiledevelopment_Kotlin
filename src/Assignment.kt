2bceb76026b8577207a8853008f5b72f609c051f
irenenyatichi <irenenyatichik@gmail.com>
2021-02-21 17:56:05 +0300

    Assignment1Variables

--- src/Assignment.kt
+++ src/Assignment.kt
@@ -1,12 +1,13 @@
 fun main() {
     val student= arrayOf("ireneNyatichi","0712345678","false",60.12345,20)
     val name=("irene_Nyatichi")
-    print(name)
+    println(name)
     var age= 20
-    print(age)
+    println(age)
     val number=("0712345678")
-    print(number)
+    println(number)
     var weight= 60.12345
-    print(weight)
-    val kenyan= (false)
+    println(weight)
+    val citizen:Boolean= (false)
+    println(citizen)
 }
